28bc214dc8179b81375f847c3a735823e7827add
Talhaimran03 <19078@studenti.marconiverona.edu.it>
2023-05-01 13:13:10 +0200

    correction ORDER BY idText

--- backend/model/ModelTexts.php
+++ backend/model/ModelTexts.php
@@ -5,7 +5,7 @@ class ModelTexts {
 
   static function get_by_level($level) {
     global $conn;
-    $query = "SELECT * FROM texts WHERE level='$level' ORDER BY dateAttempt ASC";
+    $query = "SELECT * FROM texts WHERE level='$level' ORDER BY idText ASC";
     $result = mysqli_query($conn, $query);
     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
     return $data;
