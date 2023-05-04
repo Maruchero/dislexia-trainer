f3f5c4b5462c813d9246da7c2a0e77c6bc5549d5
RiccardoVillardi <120247714+RiccardoVillardi@users.noreply.github.com>
2023-05-04 09:27:48 +0200

    Update index.php

--- backend/.initdb/index.php
+++ backend/.initdb/index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dislexia-trainer";
+$dbname = "dislexia_trainer";
 
 $conn = new mysqli($servername, $username, $password);
 mysqli_set_charset($conn, "utf8");
@@ -20,11 +20,11 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->select_db($dbname);
 
-$sql = "CREATE TABLE `users` (
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
       `username` VARCHAR(255) PRIMARY KEY,
       `password` VARCHAR(255) NOT NULL,
       `name` VARCHAR(255) NOT NULL,
-      `surname` VARCHAR(255) NOT NULL,
+      `surname` VARCHAR(255) NOT NULL
 );";
 
 if ($conn->query($sql) === TRUE) {
@@ -33,7 +33,7 @@ if ($conn->query($sql) === TRUE) {
   echo "Errore nella creazione della tabella users: " . $conn->error;
 }
 
-$sql = "CREATE TABLE `texts` (
+$sql = "CREATE TABLE IF NOT EXISTS `texts` (
       `idText` CHAR(5) PRIMARY KEY,
       `text` TEXT NOT NULL,
       `level` INT NOT NULL
@@ -45,7 +45,7 @@ if ($conn->query($sql) === TRUE) {
     echo "Errore nella creazione della tabella texts: " . $conn->error;
 }
 
-$sql = "CREATE TABLE `attempts` (
+$sql = "CREATE TABLE IF NOT EXISTS `attempts` (
       `idAttempt` CHAR(5) PRIMARY KEY,
       `username` VARCHAR(255),
       `idText` CHAR(5),
